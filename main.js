903eb169986077c48ecce08f66b7ade3144c8362
Scott Bergler <commandinghands@gmail.com>
2021-07-20 16:22:57 -0700

    As a user I want to be able to ADD two numbers

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 const display = document.getElementById("display");
-const numbers = document.getElementById("numbers");
-numbers.addEventListener("click", enterValue);
-function enterValue(e) {
-  display.textContent += e.target.value;
+const keypad = document.getElementById("keypad");
+
+keypad.addEventListener("click", handleEntry);
+
+let result = 0;
+let nextOperation = null;
+let displayString = "";
+
+function handleEntry(e) {
+  if (e.target.className === "operation") {
+    nextOperation = e.target.value;
+  }
+
+  if (e.target.className === "number") {
+    nextOperation === null
+      ? enterValue(e.target.value)
+      : operations[nextOperation](e.target.value);
+  }
 }
+
+function enterValue(value) {
+  displayString += value;
+  display.textContent = displayString;
+  result = Number(displayString);
+}
+
+function add(value) {
+  result += Number(value);
+  display.textContent = result;
+  nextOperation = null;
+  displayString = "";
+}
+
+const operations = {
+  "+": add,
+};
