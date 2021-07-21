45a5db1f37aca96ce08e163f9b2d93997ebda522
Scott Bergler <commandinghands@gmail.com>
2021-07-21 08:51:35 -0700

    As a user I want to be able to SUBTRACT two numbers

--- main.js
+++ main.js
@@ -1,38 +1,81 @@
 class Calculator {
-  result = 0;
   displayString = "";
+  result = 0;
+  firstOperand = 0;
+  secondOperand = 0;
   nextOperation = null;
+  isFirstOperand = true;
   operations = {};
 
   constructor(display) {
     this.display = display;
     this.operations["+"] = this.add;
+    this.operations["-"] = this.subtract;
   }
 
   handleEntry = (e) => {
     if (e.target.className === "operation") {
-      this.nextOperation = e.target.value;
+      this.handleOperator(e.target.value);
     }
 
     if (e.target.className === "number") {
-      this.nextOperation === null
-        ? this.enterValue(e.target.value)
-        : this.operations[this.nextOperation](e.target.value);
+      this.handleNumber(e.target.value);
     }
+    this.logTerms();
   };
 
-  enterValue = (value) => {
+  handleNumber(value) {
+    this.updateDisplay(value);
+    if (this.isFirstOperand) {
+      this.firstOperand = Number(this.displayString);
+    } else {
+      this.secondOperand = Number(this.displayString);
+    }
+  }
+
+  updateDisplay = (value) => {
     this.displayString += value;
     this.display.textContent = this.displayString;
-    this.result = Number(this.displayString);
   };
 
-  add = (value) => {
-    this.result += Number(value);
-    this.display.textContent = this.result;
-    this.nextOperation = null;
+  handleOperator(operator) {
     this.displayString = "";
+    if (this.isFirstOperand) {
+      this.nextOperation = operator;
+      this.isFirstOperand = !this.isFirstOperand;
+    } else {
+      this.handleOperation(this.nextOperation);
+      this.nextOperation = operator;
+    }
+  }
+
+  handleOperation(operator) {
+    this.result = this.operations[operator](
+      this.firstOperand,
+      this.secondOperand
+    );
+    this.updateDisplay(this.result);
+    this.firstOperand = this.result;
+    this.displayString = "";
+  }
+
+  add = (a, b) => {
+    return Number(a) + Number(b);
   };
+
+  subtract = (a, b) => {
+    return Number(a) - Number(b);
+  };
+
+  logTerms() {
+    console.log("USING 1st OP", this.isFirstOperand);
+    console.log("1st OP", this.firstOperand);
+    console.log("2nd OP", this.secondOperand);
+    console.log("OPERATOR", this.nextOperation);
+    console.log("RESULT", this.result);
+    console.log("DISPLAY", this.displayString);
+    console.log();
+  }
 }
 
 const calculator = new Calculator(document.getElementById("display"));
